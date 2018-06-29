d4aa3ab6c8633301b0ff93bc72e10ea18297a9e5
Anthony Lee <ant.lee813@gmail.com>
2018-06-29 01:04:52 -0700

    Minor var change

--- cleanLeagueLogs_Mac.py
+++ cleanLeagueLogs_Mac.py
@@ -1,7 +1,7 @@
 import glob
 import shutil
 
-dirs = glob.glob(r"/Applications/League of Legends.app/Contents/LoL/Logs/*")
+filepath = glob.glob(r"/Applications/League of Legends.app/Contents/LoL/Logs/*")
 
-for directory in dirs:
+for directory in filepath:
     shutil.rmtree(directory)
